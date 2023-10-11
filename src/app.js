fbe4c2413b7da4e9179b4da94cab0bb130890e46
Arucha27 <aruchaeduardo@gmail.com>
2023-10-11 08:56:18 -0600

    avanzando...

--- src/app.js
+++ src/app.js
@@ -7,6 +7,15 @@ const mysql = require('mysql');
 const app = express();
 app.set('port', 4000);
 
+ app.use(myconnection (mysql, {
+ host: 'localhost',
+ user: 'root',
+ password:'josue',
+ port: 3306,
+ database:''
+ }));
+
+
 app.listen(app.get('port'), () => {
     console.log('Listening on port', app.get('port'));
 });
