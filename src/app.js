182207f54a6567669025652b2be079e0144a3496
Arucha27 <aruchaeduardo@gmail.com>
2023-10-11 20:37:46 -0600

    terminado

--- src/app.js
+++ src/app.js
@@ -5,30 +5,38 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const tasksRoutes = require('./routes/tasks');
 
+
 const app = express();
 app.set('port', 4000);
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
 app.set('views', __dirname + '/views');
 app.engine('.hbs', engine({
-extname: '.hbs'
+  extname: '.hbs',
 }));
-
 app.set('view engine', 'hbs');
 
 app.use(myconnection (mysql, {
 host: 'localhost',
 user: 'root',
-password: "adminroot",
+password: "josue",
 port: 3306,
 database: 'crudfjs'
-}));
+},'single'));
+
 
 app.listen(app.get('port'), () => {
-console.log('Listening on port', app.get('port'));
+  console.log('Listening on port ', app.get('port'));
 });
+
+
 
 app.use('/', tasksRoutes);
 
 app.get('/', (req, res) => {
-res.render('home');
+  res.render('home');
 });
